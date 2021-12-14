ee6562014dcb0cf8d2743b7d5cc1fb2c0e7f2ca2
emptywork <22065214+EmptyWork@users.noreply.github.com>
2021-12-14 22:35:53 +0900

    Update Day2 to include array and string

--- src/app/netlify/emptywork/Day2module5to6/Main.java
+++ src/app/netlify/emptywork/Day2module5to6/Main.java
@@ -2,6 +2,40 @@ package app.netlify.emptywork.Day2module5to6;
 
 public class Main {
     public static void main(String[] args) {
+        // Array
+        String[] dataNama = {"Jason", "Arli", "Keyno", "Keylo", "Jacson", "Albert", "Kayla", "Yuna", "Liia", "Risya"};
+        int panjangDataNama = dataNama.length;
+
+        System.out.print("Panjang dari dataNama adalah " + panjangDataNama + "\ndan terdiri dari elemen: ");
+        for(int i = 0; i < panjangDataNama; i++) {
+            if(i == panjangDataNama - 1) {
+                System.out.print(dataNama[i] + "\n");
+            } else {
+                System.out.print(dataNama[i] + ", ");
+            }
+        }
+
+        // Metode kedua untuk deklarasi array
+        int[] datas;
+        datas = new int[2];
+
+        datas[0] = 7;
+        datas[1] = 4;
+
+        // Foreach data in datas
+        for (int data : datas) {
+            System.out.println(data);
+        }
+
+        // String
+        String kalimatSelamatDatang = "Selamat Datang Kesini!";
+        if(kalimatSelamatDatang.length() < 20) {
+            System.out.println(kalimatSelamatDatang.length() + " lebih kecil dari 20");
+        } else {
+            System.out.println(kalimatSelamatDatang.length() + " lebih besar dari 20");
+        }
+
+        // Pengenalan awal Objek
         Manusia albert = new Manusia();
         albert.setNama("Albert Andi");
         albert.setUmur(12);
